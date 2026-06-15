5d199cf05e53fbd337be7b50a65275258acfb77c
Serg <2778665@gmail.com>
2026-06-16 00:36:02 +0300

    refactor: React.memo for all components

--- src/components/country-card/country-card.tsx
+++ src/components/country-card/country-card.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { memo, useMemo } from 'react';
 import type { Country } from '../../types';
 import { DataTable } from '../data-table/data-table';
 import {
@@ -16,7 +16,7 @@ type CountryCardProps = {
   selectedColumns: string[];
 };
 
-export const CountryCard = ({ country, selectedYear, selectedColumns }: CountryCardProps) => {
+export const CountryCard = memo(({ country, selectedYear, selectedColumns }: CountryCardProps) => {
   const yearDataMap = useMemo(() => createYearDataMap(country.data), [country.data]);
   const population = useMemo(() => getPopulationForYear(yearDataMap, selectedYear), [yearDataMap, selectedYear]);
   const co2 = useMemo(() => getCo2ForYear(yearDataMap, selectedYear), [yearDataMap, selectedYear]);
@@ -40,4 +40,4 @@ export const CountryCard = ({ country, selectedYear, selectedColumns }: CountryC
       <DataTable data={country.data} year={selectedYear} columns={selectedColumns} />
     </div>
   );
-};
+});
